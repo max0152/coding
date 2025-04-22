ac95716db2cb6db8c282e1beb2fd22d35dcc97c6
max0152 <maksimmotaev5555@gmail.com>
2025-04-22 20:20:47 +0500

    изменил код для вычисления общей цены продуктов на складе
    добавил функцию которая выводит общую цену продуктов на складе путём умножения цены на количество

--- products.py
+++ products.py
@@ -5,7 +5,14 @@ def product80(products):
         ima, cena, kolvo = product
         if cena < 80:
             print(f"Товар: {ima}, Цена: {cena}, Количество:{kolvo}")
+def globalcena(products):
+    for product in products:
+        ima, cena, kolvo = product
+        totalcena = cena * kolvo
+        if kolvo * cena != 0:
+            print(f"Общая стоимость товара:{totalcena}")
 for product in products:
     ima, cena, kolvo = product
     print(f"Товар: {ima}, Цена: {cena}, Количество:{kolvo}")
 product80(products)
+globalcena(products)
