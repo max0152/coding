cd85688f675c3442fd56a8c04773f379bf18314d
max0152 <maksimmotaev5555@gmail.com>
2025-04-22 20:48:48 +0500

    изменил код добавив новую функцию для нахождения наибольшей общей цены продуктов на складе
    Создал функцию которая перемножает цену и количество одного товара, ищет наибольшее получившееся число и выводит его

--- products.py
+++ products.py
@@ -11,8 +11,19 @@ def globalcena(products):
         totalcena = cena * kolvo
         if kolvo * cena != 0:
             print(f"Общая стоимость товара:{totalcena}")
+def maxglobalcena(products):
+    max_totalcena = 0
+    max_ima = ""
+    for product in products:
+        ima, cena, kolvo = product
+        totalcena = cena * kolvo
+        if totalcena > max_totalcena:
+            max_totalcena = totalcena
+            max_ima = ima    
+    print(f"Самая дорогая общая цена на складе: Товар: {max_ima}, Общая стоимость: {max_totalcena}")
 for product in products:
     ima, cena, kolvo = product
     print(f"Товар: {ima}, Цена: {cena}, Количество:{kolvo}")
 product80(products)
 globalcena(products)
+maxglobalcena(products)
