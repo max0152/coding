9a929eda6bf93a22a4783613da4670987f7eceb0
max0152 <maksimmotaev5555@gmail.com>
2025-04-22 20:00:59 +0500

    изменил код добавив новую функцию
    добавил в код функцию которая выводит отдельно список продуктов цена которых меньше 80

--- products.py
+++ products.py
@@ -1,3 +1,11 @@
+products = ("Хлеб", 40), ("Молоко", 60), ("Яблоки", 100)
+def product80(products):
+    print("Список продуктов с ценой меньше 80:")
+    for product in products:
+        ima, cena = product
+        if cena < 80:
+            print(f"Товар: {ima}, Цена: {cena}")
 for product in products:
-    name, price = product
-    print(f"Товар: {name}, Цена: {price}")
+    ima, cena = product
+    print(f"Товар: {ima}, Цена: {cena}")
+product80(products)
