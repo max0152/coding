64ce423b442a3f829c991a25a1efd5bd1915e103
max0152 <maksimmotaev5555@gmail.com>
2025-04-22 20:05:17 +0500

    изменил код добавив новый элемент кортежа
    добавил к списку продуктов помимо цены и названия ещё и количество на складе

--- products.py
+++ products.py
@@ -2,10 +2,10 @@ products = ("Хлеб", 40), ("Молоко", 60), ("Яблоки", 100)
 def product80(products):
     print("Список продуктов с ценой меньше 80:")
     for product in products:
-        ima, cena = product
+        ima, cena, kolvo = product
         if cena < 80:
-            print(f"Товар: {ima}, Цена: {cena}")
+            print(f"Товар: {ima}, Цена: {cena}, Количество:{kolvo}")
 for product in products:
-    ima, cena = product
-    print(f"Товар: {ima}, Цена: {cena}")
+    ima, cena, kolvo = product
+    print(f"Товар: {ima}, Цена: {cena}, Количество:{kolvo}")
 product80(products)
