fd92b169b6b105b00749ef63a0615a19c24f2191
max0152 <maksimmotaev5555@gmail.com>
2025-06-03 19:22:24 +0500

    обновил код добавив новую функцию
    добавил функцию вывода списка животных в приюте

--- animal.py
+++ animal.py
@@ -18,6 +18,21 @@ class shelter:
         
     def add_animal(self, animal):
         self.animals.append(animal)
+
+    def show_animals(self):
+        if not self.animals:
+            print("Приют пуст")
+        else:
+            for animal in self.animals:
+                print(animal.display_info())
+                print()
         
+shelter = shelter()
 cat = Animal("barsik", "manchkin", 4)
-print(cat.display_info())
+cat = Animal("barsik", "manchkin", 4)
+dog = Animal("bobik", "ovcharka", 15)
+bird = Animal("kesha", "vorobey", 1)
+shelter.add_animal(cat)
+shelter.add_animal(dog)
+shelter.add_animal(bird)
+shelter.show_animals()
