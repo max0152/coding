69bbe1c517f11c069d3b036aea368ef775690c3f
max0152 <maksimmotaev5555@gmail.com>
2025-06-03 20:22:06 +0500

    время кончилось
    (

--- animal.py
+++ animal.py
@@ -2,32 +2,33 @@ class Animal:
     name = ''
     species = ''
     age = 0
-    
-    def __init__(self,name, species, age):
+    id_counter  = 1
+    def __init__(self,name, species, age,):
         self.name = name
         self.species = species
         self.age = age
-        
+        self.id = Animal.id_counter 
+        Animal.id_counter += 1
+    
     def display_info(self):
-        return f"имя: {self.name} \nразновидность: {self.species} \nвозраст: {self.age}"
-        
+        return f"имя: {self.name} \nразновидность: {self.species} \nвозраст: {self.age} \nайди: "
+    
     def get_name(self):
         return self.__name
-        
     def get_species(self):
         return self.__species
-        
     def get_age(self):
         return self.__age
-        
-class shelter:  
+    def get_id(self):
+        return self.__id
     
+class shelter:  
     def  __init__(self):
         self.animals = []
         
     def add_animal(self, animal):
         self.animals.append(animal)
-
+        
     def show_animals(self):
         if not self.animals:
             print("Приют пуст")
@@ -35,6 +36,7 @@ class shelter:
             for animal in self.animals:
                 print(animal.display_info())
                 print()
+                
     def find_by_species(self, species_name):
         poisk_animals = [animal for animal in self.animals if animal.species == species_name]
         if not poisk_animals:
@@ -53,10 +55,9 @@ class shelter:
                 found = True
                 break 
         if not found:
-            print(f"Животное '{name}' не найдено.")   
-            
+            print(f"Животное '{name}' не найдено.")
+        
 shelter = shelter()
-cat = Animal("barsik", "manchkin", 4)
 cat = Animal("barsik", "manchkin", 4)
 dog = Animal("bobik", "ovcharka", 15)
 bird = Animal("kesha", "vorobey", 1)
@@ -64,10 +65,8 @@ shelter.add_animal(cat)
 shelter.add_animal(dog)
 shelter.add_animal(bird)
 shelter.show_animals()
-shelter.remove_animal_by_name("bobik")
-shelter.remove_animal_by_name("morphling")
 print("поиск по виду 'manchkin':")
 shelter.find_by_species("manchkin")
-print("Доступ к атрибутам через методы:")
-for animal in shelter.animals:
-    print(f"Имя: {animal.get_name()}")
+shelter.show_animals()
+shelter.remove_animal_by_name("bobik")
+shelter.remove_animal_by_name("masha")
