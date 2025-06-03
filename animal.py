4c6a91849e59582bb070a77cbbd891881621ae81
max0152 <maksimmotaev5555@gmail.com>
2025-06-03 19:08:37 +0500

    обновил код добавив новый класс
    добавил класс приют хранящий в себе список животных

--- animal.py
+++ animal.py
@@ -2,12 +2,18 @@ class Animal:
     name = ''
     species = ''
     age = 0
+    
     def __init__(self,name, species, age):
         self.name = name
         self.species = species
         self.age = age
+        
     def display_info(self):
         return f"имя: {self.name} \nразновидность: {self.species} \nвозраст: {self.age}"
+
+class shelter:  
+    def  __init__(self):
+        self.animals = []
         
 cat = Animal("barsik", "manchkin", 4)
 print(cat.display_info())
