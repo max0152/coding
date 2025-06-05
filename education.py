a55113f56d9d8b65ff6d276bf0f91e2080637369
max0152 <maksimmotaev5555@gmail.com>
2025-06-05 18:58:44 +0500

    обновил код добавив новый класс
    обновил код. создав новый класс группа который хранит в себе список студентов

--- education.py
+++ education.py
@@ -6,6 +6,9 @@ class Student:
         self.student_id = student_id
     def display_info(self):
         return f"Имя: {self.name} \nайди: {self.student_id}"
+class Group:
+    def __init__(self):
+        self.students = []
 
 misha = Student("Michael", 123)
 print(misha.display_info())
