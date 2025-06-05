3324c2bc5c6ad5c964c747cf9fc68656c752072e
max0152 <maksimmotaev5555@gmail.com>
2025-06-05 19:06:49 +0500

    обновил код добавив новую функцию
    изменил код добавив функцию показа информации о группе

--- education.py
+++ education.py
@@ -1,18 +1,30 @@
 class Student:
     name = ''
     student_id = 0
+    
     def __init__(self,name,student_id):
         self.name = name
         self.student_id = student_id
+        
     def display_info(self):
         return f"Имя: {self.name} \nайди: {self.student_id}"
 class Group:
     def __init__(self):
         self.students = []
+        
     def add_student(self, student):
         self.students.append(student)
         
-group = group()
-group.add_student(misha)
+    def show_students(self):
+        if not self.students:
+            print("группа пуста")
+        else:
+            for student in self.students:
+                print(student.display_info())
+                print()
+                
 misha = Student("Michael", 123)
 print(misha.display_info())
+group = group()
+group.add_student(misha)
+group.show_students()
