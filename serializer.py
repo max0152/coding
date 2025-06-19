b0841ce8da36867af4f7ea2b5844f862c3b2d437
max0152 <maksimmotaev5555@gmail.com>
2025-06-19 18:49:51 +0500

    обновил код добавив меню
    добавил бесконечное меню в котором можно добавить новую книгу, удалить существующую, вывести информацию о всех книгах, или выйти из меню

--- serializer.py
+++ serializer.py
@@ -43,6 +43,32 @@ lib.add_book(book2)
 
 lib.show_books()
 
-lib.delete_book("Война и мир")
+while True:
+    print("Выберите действие:")
+    print("1. Добавить книгу")
+    print("2. Удалить книгу")
+    print("3. Вывести список книг")
+    print("0. Выйти")
+    choice = input("Введите номер действия: ")
 
-lib.show_books()
+    if choice == '1':
+        name = input("Введите название книги: ")
+        author = input("Введите автора: ")
+        year_input = input("Введите год издания: ")
+        try:
+            year = int(year_input)
+            new_book = Book(name, author, year)
+            lib.add_book(new_book)
+            print(f"Книга '{name}' добавлена.")
+        except ValueError:
+            print("Некорректный год. Попробуйте снова.")
+    elif choice == '2':
+        name = input("Введите название книги для удаления: ")
+        lib.delete_book(name)
+    elif choice == '3':
+        lib.show_books()
+    elif choice == '0':
+        print("Выход из программы.")
+        break
+    else:
+        print("Некорректный выбор. Попробуйте снова.")
