7fb4baa98b6d3058d14542260cb62597c38798d0
max0152 <maksimmotaev5555@gmail.com>
2025-07-08 19:25:24 +0500

    обновил код добавив тесты
    изменил код и добавил тесты для проверки правильной работы методов

--- test_unit.py
+++ test_unit.py
@@ -1,3 +1,6 @@
+import unittest
+from unittest.mock import patch
+import io
 class math:
   @staticmethod
   def fac(n):
@@ -18,3 +21,24 @@ class math:
   def form(n):
     return n + 1
     
+class TestMathMethods(unittest.TestCase):
+
+    def test_fac(self):
+        self.assertEqual(math.fac(0), 1)
+        self.assertEqual(math.fac(5), 120)
+
+    def test_step(self):
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            math.step(2, 3)
+            self.assertIn('8', mock_stdout.getvalue())
+
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            math.step(10, 2)
+            self.assertIn('100', mock_stdout.getvalue())
+
+    def test_form(self):
+        self.assertEqual(math.form(4), 5)
+        self.assertEqual(math.form(-10), -9)
+
+if __name__ == '__main__':
+    unittest.main()
