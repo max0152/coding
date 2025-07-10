befcd802f8c1510569d4632866a8c23c4ebcef4a
max0152 <maksimmotaev5555@gmail.com>
2025-07-10 19:37:09 +0500

    обновил код используя иной метод
    изменил код, теперь каждому слову можно задавать своё количество времени через которое оно выведется

--- async.py
+++ async.py
@@ -1,19 +1,39 @@
 import time
-words = [
-  'apple',
-  'axe',
-  'morphling',
-  'clarity',
-  'tango',
-  'iron',
-  'pen',
-  'assassin',
-  'watermellon',
-  'change']
+import asyncio
 
-x = -1
+async def say_hi(name, delay):
+  await asyncio.sleep(delay)
+  print(f"hi {name} after {delay} seconds!")
+  
+async def main():
+  await asyncio.gather(
+    say_hi("apple", 1),
+    say_hi("axe", 2),
+    say_hi("morphling", 3),
+    say_hi("clarity", 4),
+    say_hi("tango", 5),
+    say_hi("iron", 1),
+    say_hi("pen", 2),
+    say_hi("assassin", 3),
+    say_hi("watermellon", 4),
+    say_hi("change", 5),)
+    
+# asyncio.run(main())
+# words = [
+#   'apple',
+#   'axe',
+#   'morphling',
+#   'clarity',
+#   'tango',
+#   'iron',
+#   'pen',
+#   'assassin',
+#   'watermellon',
+#   'change']
 
-while x < 10:
-  x += 1
-  time.sleep(1)
-  print(words[x])
+# x = -1
+
+# while x < 10:
+#   x += 1
+#   time.sleep(1)
+#   print(words[x])
