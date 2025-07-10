b5b160da3d2b02549f856c42ab2b4d6d249d3ca3
max0152 <maksimmotaev5555@gmail.com>
2025-07-10 19:31:22 +0500

    написал код для списка слов
    написал код, создал список слов и заставил их выводиться по очереди с задержкой в 1 секунду

---
+++ async.py
@@ -0,0 +1,19 @@
+import time
+words = [
+  'apple',
+  'axe',
+  'morphling',
+  'clarity',
+  'tango',
+  'iron',
+  'pen',
+  'assassin',
+  'watermellon',
+  'change']
+
+x = -1
+
+while x < 10:
+  x += 1
+  time.sleep(1)
+  print(words[x])
